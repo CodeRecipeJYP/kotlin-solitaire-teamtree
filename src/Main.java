f6c3cd9fc0e3e8cac323e2282809da03c54e0087
kujyp <pjo901018@korea.ac.kr>
2018-02-26 09:25:45 +0900

    Convert Card.java into Card.kt

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Card dia = new Card(1, "dia", false);
+        System.out.println(dia);
     }
 }
