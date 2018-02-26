db9bd36fbe67399ec8c65916e02d6031b1121642
kujyp <pjo901018@korea.ac.kr>
2018-02-26 09:35:51 +0900

    initialize val in init block

--- src/Card.kt
+++ src/Card.kt
@@ -1,5 +1,11 @@
-class Card {
-    val value: Int = 0
+class Card(value: Int, suit: String, faceUp: Boolean) {
+//    raise error
+    val value: Int
     val suit: String = ""
     var faceUp: Boolean = false
+
+    init {
+        // remove above error
+        this.value = value
+    }
 }
