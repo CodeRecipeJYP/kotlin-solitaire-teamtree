0ba25458c202cf587f1324bb5d0040c0eaf9fea6
kujyp <pjo901018@korea.ac.kr>
2018-02-26 09:36:33 +0900

    Add val val var into constructor

--- src/Card.kt
+++ src/Card.kt
@@ -1,11 +1,2 @@
-class Card(value: Int, suit: String, faceUp: Boolean) {
-//    raise error
-    val value: Int
-    val suit: String = ""
-    var faceUp: Boolean = false
-
-    init {
-        // remove above error
-        this.value = value
-    }
+class Card(val value: Int, val suit: String, var faceUp: Boolean) {
 }
